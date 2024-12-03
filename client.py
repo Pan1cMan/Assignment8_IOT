7f8d58310eaef42663dc85c2bc8559697fe60c79
Pan1cMan <105896583+Pan1cMan@users.noreply.github.com>
2024-12-02 16:10:59 -0800

    input goes brrr

--- client.py
+++ client.py
@@ -12,17 +12,23 @@ def tcp_client():
 
             # Connect to the server
             client_socket.connect((server_ip, server_port))
-            print("Connected to the server.")
+            print("Connected to the server.\n")
 
             # Infinite loop to keep sending requests
             while True:
-                # Input request from the user
-                message = input("Enter a request (or type 'exit' to quit): ")
 
-                # Exit if the user types 'exit'
+                # Given options to the user to enter for the server to response
+                directions = '''Request a response from the server by inputting the following number:\n1. What is the average moisture inside my kitchen fridge in the past three hours?\n2. What is the average water consumption per cycle in my smart dishwasher?\n3. Which device consumed more electricity among my three IoT devices (two\nrefrigerators and a dishwasher)? '''
+                # Input request from the user
+                message = input(f"{directions}\nEnter a request (or type 'exit' to quit): ")
+
+                # Exit if the user types 'exit '
                 if message.lower() == "exit":
                     print("Closing the connection.")
                     break
+                elif message not in {"1", "2", "3"}:
+                    print("\nSorry, this query cannot be processed. Please try one of the following:")
+                    continue
 
                 # Send the request to the server
                 client_socket.sendall(message.encode())
@@ -31,7 +37,7 @@ def tcp_client():
                 response = client_socket.recv(1024)
 
                 # Print the response from the server
-                print(f"Response from server: {response.decode()}")
+                print(f"\nResponse from server: {response.decode()}\n")
 
     except ValueError:
         print("Error: Invalid port number. Please enter an integer.")
